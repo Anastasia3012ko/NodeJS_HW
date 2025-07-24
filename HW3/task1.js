a4cd97f7cd18845e217021c40bce877e58c09127
AnastasiK30 <anastasia301284@gmail.com>
2025-07-24 14:22:41 +0200

    homework 3 done

---
+++ HW3/task1.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const dirPath = path.join(__dirname, 'myFolder')
+
+fs.mkdir(dirPath, error => {
+    if(error) {
+        console.error('Error: ', error)
+        return
+    }
+    console.log('Folder "myFolder" created')
+})
+fs.rmdir(dirPath, error => {
+    if(error) {
+        console.error('Error: ', error)
+        return
+    }
+    console.log('Folder "myFolder" deleted')
+})
---
+++ HW3/task2.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+fs.writeFile('info.txt', 'Node.js is awesome!', 'utf-8', err => {
+    if(err) {
+        console.log('Error writing file: ', err)
+        return
+    }
+    console.log('File created and info recorded')
+})
+fs.readFile('info.txt', 'utf-8', (err, data) => {
+    if(err) {
+        console.log('Error reading file: ', err)
+        return
+    }
+    console.log('Info from file: ', data)
+})
