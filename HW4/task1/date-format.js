7a8f1aacb7e7ef5cae796ee808cb0fc8051bcac3
AnastasiK30 <anastasia301284@gmail.com>
2025-08-12 21:57:12 +0200

    added homework 4

---
+++ HW4/task1/date-format.js
@@ -0,0 +1,6 @@
+import moment from 'moment';
+
+const dateFormatOne = moment().format('DD-MM-YYYY');
+const dateFormatTwo = moment().format('MMM Do YY');
+const dateFormatThree = moment().format('dddd');
+console.log(`Today is ${dateFormatOne} or ${dateFormatTwo}, Day of the week: ${dateFormatThree}/n`);
---
+++ HW4/task2/file-operation.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const fileName = process.env.FILENAME;
+
+fs.writeFile(fileName, 'My homework 4 task2!!!!', 'utf-8', (error)=>{
+    if(error) {
+        console.log('Error with writing to  file', error);
+        return;
+    }
+    console.log('Data written to the file successfully');
+    fs.readFile(fileName, 'utf-8', (data, error)=>{
+        if(error) {
+        console.log('Error reading file', error);
+        return;
+    }
+    console.log('File read successfully: ', data);
+    })
+})
